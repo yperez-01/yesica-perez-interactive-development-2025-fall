5babfd944e07a29b058adb41a7a629a3e1678c29
yperez-01 <yesica.mperez01@gmail.com>
2025-10-23 18:29:12 -0500

    js added

---
+++ harmonic-collection/js/script.js
@@ -0,0 +1,25 @@
+ /*const targetElement = document.getElementById('problem');
+ const textContent = targetElement.textContent;
+    const words = textContent.split('problem');
+
+    let newHTML = '';
+    words.forEach(word => {
+        newHTML += `<span>${problem}</span> `; // Add a space after each word
+    });
+    targetElement.innerHTML = newHTML;
+
+    // Using GSAP (after including the library)
+        gsap.from(targetElement.querySelectorAll('problem'), {
+            opacity: 0,
+            y: 50,
+            stagger: 0.2, // Stagger animation by 0.1 seconds
+            duration: 0.8
+        });*/
+
+
+$("span").on("mouseenter", function(){
+    $(this).css({"color":"red"})
+});
+$("span").on("mouseleave", function(){
+    $(this).css({"background-color": "lightblue"});
+  });
