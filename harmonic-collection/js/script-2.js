0944ad545b2d22197b7cd2e1b6c5dac64b955adf
yperez-01 <yesica.mperez01@gmail.com>
2025-10-30 20:13:17 -0500

    audio link add

--- harmonic-collection/js/script-2.js
+++ harmonic-collection/js/script-2.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded') , () => {
   const animatedText = document.querySelector('.content');
 }
+
+// Get the span and audio elements by their IDs
+const clickMe = document.getElementById(".effect");
+const soundEffect = document.getElementById("sound-effect");
+
+// Add a click event listener to the span
+clickMe.addEventListener("hover", () => {
+  // Play the sound when the span is clicked
+  soundEffect.play();
+});
